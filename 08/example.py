6533edfbb519f29ec4cc5a9c81ceaac8ff3e0e44
Serhii Tytarenko <serhii.tytarenko@gmail.com>
2022-12-12 17:24:37 +0200

    Lecture 8

---
+++ 08/example.py
@@ -0,0 +1,104 @@
+#
+import sys
+
+
+def check(a):
+    return a > 0
+
+class Test:
+    a = Desct()
+
+    def __init__(self, b):
+        if not check(b):
+            self.b = b
+        else:
+            print('Error', file=sys.stderr)
+
+a = 0
+
+test = Test(10)
+test.b = a
+test.a = a
+
+class Math:
+
+    def div(self, a, b):
+        if b != 0:
+            return a / b
+        else:
+            print('Error', file=sys.stderr)
+
+
+
+
+
+
+class NonNegative:
+    def __set_name__(self, owner, name):
+        self.name = f'__{name}'
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        assert value > 0, 'non-negative value required'
+        setattr(instance, self.name, value)
+
+
+
+
+
+class Test1:
+    value1 = NonNegative()
+    value2 = 1
+
+
+
+test1 = Test1()
+test1.value1
+
+
+
+
+class A:
+    b = property()
+
+
+class C(object):
+    @property
+    def x(self):
+        "I am the 'x' property."
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+
+
+c = C()
+
+c.x = 10
+
+def test(a, b)
+    return a + b
+
+test(c.x(), 2)
+
+
+map(int, '1234')
+
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def readers(self):
+        return get_from_db_all_readers(self.name)
+
+
+
+book = Book('Math')
+book.readers # => ['John Dou', '...']
---
+++ 08/oop.py
@@ -0,0 +1,122 @@
+"""
+ООП
+
+Інтерфейс
+=>
+Клас
+=>
+Обʼєк
+
+
+
+Клас             Обʼєкт(Екземпляр Класу)    Обʼєкт(Python)
+поля даних  =>   властивості             => атрибут
+методи      =>   поведінка               => атрибут
+"""
+
+
+class Book:
+    # поле
+    type = 'magazine'
+
+    # метод
+    def __init__(self, name, author):
+        self.author = author
+        self.name = name
+
+    # метод
+    def get_author(self):
+        return self.author
+
+    # метод
+    def get_name(self):
+        return self.name
+
+
+book = Book('John', 'Math')
+print(book.type)
+print(book.get_author())
+
+
+
+"""
+Принципи ооп
+
+1. Абстакція
+2. Інкапсуляція
+3. Наслідування
+4. Поліморфізм
+
+
+Абстакція
+Інкапсуляція
+Наслідування
+Поліморфізм
+
+
+"""
+
+def div(a, b):
+    return a / b
+
+
+res = div(10, 3)
+
+
+class A:
+    def test(self):
+        return 43
+
+
+class B(A):
+    def test2(self):
+        return self.test() / 3
+
+
+
+class Client:
+    def get(self, site):
+        response = send_request(site)
+        return response.text
+
+
+class ClientBBC(Client):
+    site = 'bbc.com'
+
+    def get(self, site):
+        result = super().get(self.site)
+        return result
+
+
+# Інтерфейс
+class Figure:
+    @NotImplemented
+    def get_perimeter(self):
+        """
+        :return:
+        """
+        pass
+
+    @NotImplemented
+    def get_area(self):
+        pass
+
+class NonNegative:
+    
+
+# Класс Triangle реалізує інтерфейс Figure
+class Triangle(Figure):
+    a = NonNegative()
+    b = NonNegative()
+    c = NonNegative()
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_perimeter(self):
+        return self.a + self.b + self.c
+
+    def get_area(self):
+        return ...
